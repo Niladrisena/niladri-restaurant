dfb8b7a9c635d0938791900d6b11ed40e8f6ac92
Niladri <117145249+Niladrisena@users.noreply.github.com>
2023-12-22 22:10:54 +0530

    Restaurant landing page

---
+++ src/components/Footer/FooterOverlay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './FooterOverlay.css';
+
+const FooterOverlay = () => (
+  <div className="app__footerOverlay">
+    <div className="app-_footerOverlay-black" />
+    <div className="app-_footerOverlay-img app__bg" />
+  </div>
+);
+
+export default FooterOverlay;
